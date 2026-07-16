1dce6b886799af921c6a3e1479e36a5825bcbff7
notanoty <timariefev05@gmail.com>
2026-07-16 20:53:28 +0300

    feature: theme changer

--- bin/main.ml
+++ bin/main.ml
@@ -13,28 +13,28 @@ let height: int = 9 * window_scaler
 
 let bg_color_dark = Layout.opaque_bg (RGB.find_color "#000303") 
 let bg_color_light = Layout.opaque_bg  (RGB.find_color "#ffffff")
+let fg_color_dark  = RGBA.find_color "#eddedd"
+let fg_color_light = RGBA.find_color "#000303"
+
+let fg = ref fg_color_dark
 let bg =  ref bg_color_dark
 let is_dark_theme = ref true
-
-let temperature  = ref 0.0
+let temperature  = ref 100.0
 let my_custom_font = Label.font_from_file "asset/Dotfont-Regular.otf"
 
  (* Time *)
-
 let today = Unix.localtime (Unix.time ())
 let day = today.Unix.tm_mday
-
-let hour = today.tm_hour
-let minute = today.tm_min
-let second = today.tm_sec
+let hour = 99
+let minute = 99 
+let second = 99 
 
 (* Widgets *)
 let temperature_lable = Widget.label 
-~fg:(RGBA.find_color "#eddedd") 
-~font:my_custom_font
-~size: (window_scaler / 2 )
-(Printf.sprintf "%.1fC" !temperature) 
-
+  ~fg:(RGBA.find_color "#eddedd") 
+  ~font:my_custom_font
+  ~size: (window_scaler / 2 )
+  (Printf.sprintf "%.1fC" !temperature) 
 
 let time_label = Widget.label
   ~fg:(RGBA.find_color "#eddedd") 
@@ -42,35 +42,29 @@ let time_label = Widget.label
   ~size: window_scaler  
   (Printf.sprintf "%02d:%02d:%02d" hour minute second) 
 
-
-let button = Widget.button "Test button"
-
+let button = 
+  Widget.button 
+    ~bg_off: (Style.Solid (RGBA.find_color "#ccdffc")) 
+    ~bg_over: (Some (Style.Solid (RGBA.find_color "#555555"))) 
+    ~bg_on: (Style.Solid (RGBA.find_color "#ccdffc")) 
+    "Theme L/D"
 
 (*Layout*)
-let temp_x = width - 150
+let temp_x = width - (2 * window_scaler)
 let temp_y = 10
 let temp_layout = Layout.resident ~x:temp_x ~y:temp_y temperature_lable
 
-
 let time_bounding_widget = Widget.empty ~w:(width - 23) ~h:height ()
-
 let box_layout = Layout.resident time_bounding_widget
 let label_layout = Layout.resident time_label
-
 let time_layout = Layout.superpose ~center:true [box_layout; label_layout]
 
-
-let button_x = 10
-let button_y = 10
+let button_x = 1 * window_scaler / 2
+let button_y = 1 * window_scaler / 2
 let button_layout = Layout.resident ~x:button_x ~y:button_y button 
-
-
-
 
 let main () =
   let bg_layout = Layout.resident ~w:width ~h:height (Widget.label "") in
-
-
   (* Functions *)
   let update_clock () =
   let now = Unix.localtime (Unix.time ()) in
@@ -78,36 +72,34 @@ let main () =
     Widget.set_text time_label new_text;
     Widget.update time_label in
 
-  let update_theme _src _dst _ev = 
+  let update_theme _src _dst _ev =
     if !is_dark_theme then (
       is_dark_theme := false;
-      bg := bg_color_light
+      bg := bg_color_light;
+      fg := fg_color_light
     ) else (
       is_dark_theme := true;
-      bg := bg_color_dark
+      bg := bg_color_dark;
+      fg := fg_color_dark
     );
-    (* Use Layout.set_background directly. 
-       !bg is already of type Layout.background. *)
-    Layout.set_background bg_layout (Some !bg) in
+    Layout.set_background bg_layout (Some !bg);
+    Label.set_fg_color (Widget.get_label time_label) !fg;
+    Label.set_fg_color (Widget.get_label temperature_lable) !fg;
+    Widget.update time_label;
+    Widget.update temperature_lable in
 
-
-  (* Connections *)
   let c_theme_button =
     Widget.connect ~priority:Widget.Join button time_label update_theme Trigger.buttons_down
   in
   
   Layout.set_background bg_layout (Some !bg);
-
-  let main_layout = Layout.superpose [ bg_layout; time_layout; temp_layout; button_layout] in
-
+  let main_layout = Layout.superpose ~scale_content:true
+    [ bg_layout; time_layout; temp_layout; button_layout] in
   let layout = main_layout in
-  
-  Layout.set_width layout width;
-  Layout.set_height layout height;
 
   let board = Bogue.of_layout ~connections:[c_theme_button] layout in
-
   Bogue.run ~before_display:update_clock board
+
 
 let () =
   Lwt_main.run (
@@ -121,10 +113,10 @@ let () =
     let open Yojson.Safe.Util in
     let temp = json |> member "current" |> member "temperature_2m" |> to_float in
     temperature := temp;
+    Widget.set_text temperature_lable (Printf.sprintf "%.1fC" temp);
     
     Printf.printf "The temperature is: %.1f\n" temp;
     Lwt.return ()
   );
-
   main ();
   Bogue.quit ()
